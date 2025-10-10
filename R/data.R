4e3589a746981510baa3e4439116fb9d38b265a2
fdzul <felipe.dzul.m@gmail.com>
2025-10-09 19:33:24 -0600

    this commit update the pkg

--- R/data.R
+++ R/data.R
@@ -1,18 +1,18 @@
 #' climex
 #'
-#' daily climate dataset for each municipality in Mexico
+#' climex: daily climate dataset for each municipality in Mexico
 #'
 #' @format A data.frame object with 5163223 rows and 7 variables:
 #' \describe{
 #'   \item{cvegeo}{the ids.}
-#'   \item{latitude}{the ids.}
-#'   \item{longitude}{is the numeric key of state.}
-#'   \item{date}{is the numeric key of federal district.}
-#'   \item{T2M}{is the numeric key to the Municipality}
-#'   \item{RH2M}{is the numeric key to the Sectors}
-#'   \item{PRECTOTCORR}{is numeric the key of locality}
+#'   \item{latitude}{the latitude}
+#'   \item{longitude}{the longitude}
+#'   \item{date}{is the date}
+#'   \item{T2M}{temperature 2 m high}
+#'   \item{RH2M}{relative humidity at 2 m height}
+#'   \item{PRECTOTCORR}{corrected total precipitation}
 #'   ...
 #' }
-#' @source \url{https://github.com/fdzul/ewarsmx}
+#' @source \url{https://power.larc.nasa.gov/parameters/}
 #' @details climex provides the daily climate data for each municipality in Mexico, downloaded from the [NASA Prediction Of Worldwide Energy Resources (POWER)](https://power.larc.nasa.gov) using the Python package [climex](https://github.com/fdzul/climex). climex uses the [Data Requests API](https://power.larc.nasa.gov/docs/tutorials/service-data-request/api/). The time series covers the period from January 1, 2020, to September 30, 2025.
 "climex"
