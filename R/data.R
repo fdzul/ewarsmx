5530ae94cab43d537992d9809fb3319186189ca3
fdzul <felipe.dzul.m@gmail.com>
2025-10-09 18:56:50 -0600

    this commit update the pkg

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Polygons of blocks from Mexico of the National Electoral Institute 2020
+#' climex
 #'
-#' A spatial dataset containing of polygon data of blocks from Mexico
+#' daily climate dataset for each municipality in Mexico
 #'
 #' @format A data.frame object with 5163223 rows and 7 variables:
 #' \describe{
@@ -14,5 +14,5 @@
 #'   ...
 #' }
 #' @source \url{https://pautas.ine.mx/transparencia/mapas/}
-#' @details climex provides the daily climate data base for each municipality in Mexico, downloaded from the [NASA Prediction Of Worldwide Energy Resources (POWER)](https://power.larc.nasa.gov) using the Python package [climex](https://github.com/fdzul/climex). climex uses the [Data Requests API](https://power.larc.nasa.gov/docs/tutorials/service-data-request/api/). The time series covers the period from January 1, 2020, to September 30, 2025.
+#' @details climex provides the daily climate data for each municipality in Mexico, downloaded from the [NASA Prediction Of Worldwide Energy Resources (POWER)](https://power.larc.nasa.gov) using the Python package [climex](https://github.com/fdzul/climex). climex uses the [Data Requests API](https://power.larc.nasa.gov/docs/tutorials/service-data-request/api/). The time series covers the period from January 1, 2020, to September 30, 2025.
 "climex"
